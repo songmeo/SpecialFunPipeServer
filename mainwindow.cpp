30d215995a6ddc77dd89397524aa867e65004e06
songmeo <shuong1012@gmail.com>
2020-05-28 22:25:17 +0300

    send msg works (almost)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+void MainWindow::hexprint(const QByteArray& arr) {
+    unsigned char chars[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    QString s;
+    size_t size = arr.size();
+    unsigned char c;
+    for(size_t i = 0; i < size; i++) {
+        c = arr.at(i);
+        s.append(chars[c >> 4]);
+        s.append(chars[c & 0xf]);
+    }
+    setMsg(s);
+}
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -33,21 +47,25 @@ void MainWindow::setMsg(const QString &iconText)
 
 void MainWindow::sendMsg() {
     QByteArray msg;
-    QDataStream s(&msg, QIODevice::WriteOnly);
+    QDataStream msg_str(&msg, QIODevice::WriteOnly);
 
     QByteArray size;
     QDataStream size_str(&size, QIODevice::WriteOnly);
+    msg_str.setByteOrder(QDataStream::LittleEndian);
     size_str.setByteOrder(QDataStream::LittleEndian);
-
+    // msg format:   uint32 size || utf16 f_name || double x0 || double xn || uint32 nPoints ( || order ( bessel only) )
     QString f = ui->function_input->currentText();
+    f.append(QChar(0)); //null terminator
     double x0 = ui->x0_input->toPlainText().toDouble();
     double xn = ui->xn_input->toPlainText().toDouble();
     int points = ui->points_input->toPlainText().toInt();
     int order = ui->order_input->toPlainText().toInt();
 
-    s << f << x0 << xn << points << order;
+    msg_str << f << x0 << xn << points << order;
     size_str << msg.size();
     msg.replace(0,4,size);
+
+    hexprint(msg);
 
     socket.write(msg);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void hexprint(const QByteArray& arr);
 
 private slots:
     void on_connect_button_clicked();
